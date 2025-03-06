953464eda2c6f229ffd40f9e8cf87f9fc105ad4a
Sanketpatil012 <122162948+Sanketpatil012@users.noreply.github.com>
2025-03-06 14:36:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,24 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+   let pricesofItems= document.querySelectorAll(".price")
+    let sum=0; 
+
+	pricesofItems.forEach((item) =>{
+		 sum += parseFloat(item.innerText) || 0
+	})
+
+	const tr= document.createElement('tr')
+	const td = document.createElement('td')
+
+	 td.colSpan=2; 
+	 td.innerText=`Total Price : ${sum.toFixed(2)}`
+
+	tr.appendChild(td)
+	 // Append to table body (assuming table exists)
+    const table = document.querySelector("table tbody"); // Ensure a table body exists
+     table.appendChild(tr)
+	
 };
 
 getSumBtn.addEventListener("click", getSum);
